a7635f1e6c769f427c49ca3d31bbf687014d6a81
FrozenTear7 <RainbowPablo7@gmail.com>
2018-06-02 20:43:14 +0200

    Add list of lecturers and their classes

--- project3/quickstart.py
+++ project3/quickstart.py
@@ -6,12 +6,16 @@ import re
 # its values are the wrong records and keys - row:column
 
 toCorrect = []
+lecturers = {}
 
 
 def filter_records(arr):
     new_arr = []
     for i in range(0, len(arr)):
         if arr[i]['studia'] != '---':
+            if arr[i]['osoba'] and arr[i]['osoba'] not in lecturers:
+                lecturers[arr[i]['osoba']] = []
+
             new_obj = {}
             new_obj['row'] = i + 1
             new_obj['record'] = arr[i]
@@ -26,9 +30,20 @@ def push_to_to_correct(x, error_type):
     toCorrect.append(new_obj)
 
 
+def check_day(day):
+    days = ['Pn', 'Wt', 'Sr', 'Cz', 'Pt']
+    if day not in days:
+        return False
+    else:
+        return True
+
+
 def correct_arr(arr):
     for x in arr:
         record = x['record']
+        if record['osoba']:
+            lecturers[record['osoba']].append(record)
+
         if not re.match('^s\d+$', record['studia']):
             push_to_to_correct(x, 'studia')
         elif not re.match('^\d+$', str(record['sem'])):
@@ -43,6 +58,14 @@ def correct_arr(arr):
             push_to_to_correct(x, 'wym')
         elif record['miejsce'] and not re.match('^\w\d+ \S+$', record['miejsce']):
             push_to_to_correct(x, 'miejsce')
+        elif record['tyg'] and not str(record['tyg']) in '12AB':
+            push_to_to_correct(x, 'miejsce')
+        elif record['dzien'] and not check_day(record['dzien']):
+            push_to_to_correct(x, 'wym')
+        elif record['godz'] and not re.match('^\d{2}:\d{2}$', str(record['godz'])):
+            push_to_to_correct(x, 'wym')
+        elif record['koniec'] and not re.match('^\d{2}:\d{2}$', str(record['koniec'])):
+            push_to_to_correct(x, 'wym')
 
 
 # connect to the spreadsheet
@@ -68,3 +91,7 @@ correct_arr(wks)
 
 for i in toCorrect:
     print(i)
+
+# for key in lecturers:
+#     print(key)
+#     print(lecturers[key])
